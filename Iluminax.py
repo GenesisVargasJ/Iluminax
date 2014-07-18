ab380869f6e823fd8eaf0a03f90e6191de7a1d9b
Genesis Vargas J <genesisvargasj@gmail.com>
2014-07-18 17:43:51 -0500

    Update Iluminax.py

--- Iluminax.py
+++ Iluminax.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
 # Iluminax! - Minisoftware para controlar el brillo y color de la pantalla #
 # Autor: Genesis Vargas J #
 # Website: http://www.genesisvargasj.com #
@@ -9,24 +12,21 @@ import subprocess
 
 class Iluminax():
 	
-	def DebugTrue(self):
-		return False
-	
-	def DetectarDispositivos(self):
-		disp_conectados = []
-		salida_xrandr = subprocess.check_output('xrandr -q', shell=True)
-		lineas = salida_xrandr.split('\n')
-		for line in lineas:
-			palabras = line.split(' ')
-			for word in palabras:
-				if word == 'connected':
-					disp_conectados.append(palabras[0])
-		return disp_conectados
-	
 	def __init__(self):
 		b = Gtk.Builder()
 		b.add_from_file("interfaz.glade")
-		
+		self.DispositivosDetectados = self.DetectarDispositivos()
+		self.NumDispositivosDetectados = len(self.DispositivosDetectados)
+		if self.NumDispositivosDetectados == 1:
+			self.PrimerDispositivo = self.DispositivosDetectados[0]
+		else:
+			self.PrimerDispositivo = 'No Encontrado :('
+		self.comandos_dispositivo = []
+		self.valor = 0.00
+		for i in xrange(0, 101):
+			comando_primer_dispositivo = "xrandr --output \%s --brightness %s" % (self.PrimerDispositivo, self.valor)
+			self.comandos_dispositivo.append(comando_primer_dispositivo)
+			self.valor += 0.01
 		self.FrmPrincipal = b.get_object("FrmPrincipal")
 		self.FrmInfo = b.get_object("FrmInfo")
 		self.BoxHorizontal = b.get_object("eventbox1")
@@ -39,13 +39,13 @@ class Iluminax():
 		self.FrmPrincipal.show()
 	
 	def on_BtnBajo_clicked(self, button):
-		CambiarBrilloPantalla(70, "pantalla1.png")
+		self.CambiarBrilloPantalla(70, "pantalla1.png")
 	
 	def on_BtnMedio_clicked(self, button):
-		CambiarBrilloPantalla(85, "pantalla2.png")
+		self.CambiarBrilloPantalla(85, "pantalla2.png")
 	
 	def on_BtnAlto_clicked(self, button):
-		CambiarBrilloPantalla(100, "pantalla3.png")
+		self.CambiarBrilloPantalla(100, "pantalla3.png")
 	
 	def on_BtnSalir_clicked(self, button):
 		Gtk.main_quit()
@@ -66,6 +66,18 @@ class Iluminax():
 	def on_RbtnNormal_toggled(self, button):
 		system("xrandr --output \%s --gamma %s" % (self.PrimerDispositivo, "1.0:1.0:1.0"))
 		
+	#procedimiento que hace un subproceso para detectar el dispositivo de pantalla
+	def DetectarDispositivos(self):
+		disp_conectados = []
+		salida_xrandr = subprocess.check_output('xrandr -q', shell=True)
+		lineas = salida_xrandr.split('\n')
+		for line in lineas:
+			palabras = line.split(' ')
+			for word in palabras:
+				if word == 'connected':
+					disp_conectados.append(palabras[0])
+		return disp_conectados
+	
 	#procedimiento para cambiar brillo de pantalla
 	def CambiarBrilloPantalla(self, numerobrillo, imagen):
 		system(self.comandos_dispositivo[numerobrillo])
